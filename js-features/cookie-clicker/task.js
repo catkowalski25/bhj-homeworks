73e4a9a5f87a9bc7d6f51853360fc5cdbdf59b9e
catkowalski25 <risha25@list.ru>
2023-07-04 00:13:50 +0300

    cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,20 @@
+const cookie = document.getElementById('cookie');
+let prevTime = new Date;
+console.log (prevTime.getSeconds(),prevTime.getMilliseconds())
+cookie.onclick = function (){
+const counter = document.getElementById('clicker__counter');
+const speedCounter = document.getElementById('clicker__speedCounter');
+counter.textContent = +counter.textContent + 1;
+let currTime = new Date;
+speedCounter.textContent = (1 / 100 * (currTime - prevTime)).toFixed(2);
+prevTime = currTime;
+cookieWidth = cookie.width;
+cookieHeight = cookie.height
+cookie.width += 50;
+cookie.height += 50;
+
+setTimeout(()=>{
+    cookie.width = cookieWidth;
+    cookie.height = cookieHeight;
+}, 100)
+}
