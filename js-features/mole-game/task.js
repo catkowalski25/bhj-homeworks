b8e8908342bf2d1e3b2a30d9d5251506c6eeca57
catkowalski25 <risha25@list.ru>
2023-07-04 22:51:16 +0300

    mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,11 +1,30 @@
 const deadSpan = document.getElementById('dead');
 const lostSpan = document.getElementById('lost');
-let deadCounter = + deadSpan.textContent;
-let lostCounter = + lostSpan.textContent;
+
 function holeClick(){
-   if (deadCounter>4){
-    alert("Win");
-    deadCounter = 0; 
-    lostCounter = 0;
+    let deadCounter = + deadSpan.textContent;
+    let lostCounter = + lostSpan.textContent;
+    if (this.className.includes('hole_has-mole')){
+        deadCounter++
+    } else {
+        lostCounter++
+    }
+
+    if (deadCounter > 9){
+        alert("Победа!");
+        deadSpan.textContent = 0; 
+        lostSpan.textContent = 0;
+    }else if(lostCounter > 4){
+        alert("Вы проиграли(");
+        deadSpan.textContent = 0; 
+        lostSpan.textContent = 0;
+    } else {
+        deadSpan.textContent = deadCounter;
+        lostSpan.textContent = lostCounter;
+    }
+
 }
+
+for (let index = 1; index < 10; index++) {
+    document.getElementById(`hole${index}`).onclick = holeClick;    
 }
