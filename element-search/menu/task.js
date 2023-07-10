f3e27eda4d21bf79d79f6856e2be05bd7d508c9e
catkowalski25 <risha25@list.ru>
2023-07-11 00:53:18 +0300

    menu basic

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,23 +5,10 @@ function showMenu () {
             menuSub.className = menuSub.className.replace(" menu_active", "");
         } else {
             menuSub.className += " menu_active"
-        }
-        
+        }   
     }
     return false
 }
-
 menuItem = document.querySelectorAll('.menu_main>li');
 let menuItemArr = Array.from(menuItem);
 menuItemArr.forEach(li => li.onclick = showMenu);
-    
-
-
-
-
-
-
-
-
-
-console.dir(menuItem);
