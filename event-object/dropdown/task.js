41cb3a4d10eb91e9db9a30b7c04064ab1987e3ba
catkowalski25 <136506010+catkowalski25@users.noreply.github.com>
2023-07-17 15:52:11 +0300

    dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -7,4 +7,5 @@ function clickDropdown(event) {
    let value = this.querySelector('.dropdown__value');
    clicked.closest('.dropdown__list').classList.toggle('.dropdown__list-active');
    value.innerHTML = clicked.innerHTML;
+   event.preventDefault();
 }
