0329eaebd234ee4c0da511e35aad2417ede43555
catkowalski25 <risha25@list.ru>
2023-07-24 22:41:30 +0300

    dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -6,7 +6,7 @@ function clickDropdown(event) {
    let clicked = event.target;
    let value = this.querySelector('.dropdown__value');
    let ul = this.querySelector('.dropdown__list');
-   ul.classList.toggle('.dropdown__list-active');
+   ul.classList.toggle('dropdown__list_active');
    if (clicked !== ul){value.innerHTML = clicked.textContent};
    event.preventDefault();
 }
