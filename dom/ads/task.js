2e5ccf2f19c1372e4d1f5a47d4bcfd110d6e18b0
catkowalski25 <risha25@list.ru>
2023-07-28 01:10:51 +0300

    ads hard

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,19 @@
-document.querySelectorAll(".rotator").forEach(rttr => 
-    setInterval(caseRotator, 1000, rttr))
+document.querySelectorAll(".rotator").forEach(rttr => {
+    let first = rttr.querySelector(".rotator__case_active");
+    first.style.color = first.dataset.color;
+    setTimeout(caseRotator, first.dataset.speed, rttr)
+});
 
 function caseRotator(elem) {
     let rttrCases = elem.querySelectorAll(".rotator__case");
     for (let index = 0; index < rttrCases.length; index++) {
         if (rttrCases[index].classList.contains("rotator__case_active")){
             rttrCases[index].classList.remove("rotator__case_active");
-            rttrCases[(index+1) % rttrCases.length].classList.add("rotator__case_active");
-        break;
+            let next = rttrCases[(index+1) % rttrCases.length];
+            next.classList.add("rotator__case_active");
+            next.style.color = next.dataset.color;
+            setTimeout(caseRotator, next.dataset.speed, elem)
+            break;
         }
     }
 }
