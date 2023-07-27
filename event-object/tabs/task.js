a37ac5e36825e2c5aae2f31efc2bd0691f1cbd4f
catkowalski25 <risha25@list.ru>
2023-07-27 12:48:09 +0300

    tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,11 +1,16 @@
 let tabs = document.querySelectorAll('.tab');
+let conntents = document.querySelectorAll('.tab__content');
 tabs.forEach(tab => {
     tab.addEventListener("click", clickTab)
 });
 
 function clickTab(event) {
-    tabs.forEach(tab => tab.classList.remove("tab_active"));
+    let currentIndex;
+    tabs.forEach((tab, index) => {
+        tab.classList.remove("tab_active");
+        if (tab === event.target){currentIndex = index}
+    });
+    conntents.forEach(conntent => conntent.classList.remove("tab__content_active"));
     event.target.classList.add("tab_active");
-    console.log(event);
-    console.log(event.target);
+    conntents[currentIndex].classList.add("tab__content_active");
 }
