1c1935cd73687891fa32c3e4ce8c3f2641aaf623
ZooLooS25 <s7kuznetsov@gmail.com>
2023-08-03 17:00:38 +0300

    todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,25 @@
+const taskList = document.getElementById('tasks__list');
+const taskInput = document.getElementById('task__input');
+document.getElementById('tasks__add').addEventListener('click',addTask);
+
+function addTask(event) {
+   event.preventDefault();
+   let newTaskValue = taskInput.value;
+   if (newTaskValue) {
+      taskInput.value = "";
+   } else {
+      return
+   }
+   let newTask = document.createElement('div');
+   newTask.classList.add("task")
+   newTask.innerHTML = `
+      <div class="task__title">
+         ${newTaskValue}
+      </div>
+      <a href="#" class="task__remove">&times;</a>
+      `;
+   newTask.lastElementChild.addEventListener('click', function() {
+      this.parentElement.remove();
+   });
+   taskList.insertAdjacentElement("beforeEnd", newTask);
+}
