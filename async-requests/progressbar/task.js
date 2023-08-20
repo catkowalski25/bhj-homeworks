45f1c0be6f7a65bfd9bd3b851fbecfae8aa74d8b
catkowalski25 <risha25@list.ru>
2023-08-21 01:29:39 +0300

    progressbar fixed

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,16 +4,9 @@ document.getElementById('send').addEventListener('click', (e)=>{
    let formData = new FormData(document.forms.form);
    let request = new XMLHttpRequest();
    request.open('POST', ' https://students.netoservices.ru/nestjs-backend/upload');
-   request.addEventListener('readystatechange', function() {
-      if (this.readyState === 1) {
-         progress.value = 0.25;
-      } else if (this.readyState === 2) {
-         progress.value = 0.5;
-      }else if (this.readyState === 3) {
-         progress.value = 0.75;
-      } else {
-         progress.value = 1;
-      }  
-   });
+
+   request.upload.onprogress = e => {
+      progress.value = e.loaded / e.total; 
+   };
    request.send(formData);
 });
